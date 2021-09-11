5a8705090591efb9539a0572b0508a9ba25b9e85
mad-kong <kcy0076@gmail.com>
2021-09-11 21:06:35 +0900

    baekjoon_step1

---
+++ baekjoon_step1/baekjoon_step1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	//cout << "Hello World!";
+
+	//cout << "강한친구 대한육군" << endl << "강한친구 대한육군";
+
+	//cout << "\\    /\\" << endl << " )  ( ')" << endl << "(  /  )" << endl << " \\(__)|";
+
+	//int A, B;
+	//cin >> A >> B;
+	//cout << A + B;
+
+	//int A, B;
+	//cin >> A >> B;
+	//cout << A - B;
+
+	//int A, B;
+	//cin >> A >> B;
+	//cout << A * B;
+
+	//int A, B;
+	//cin >> A >> B;
+	//cout << fixed;
+	//cout.precision(9);
+	//cout << A /(long double)B;
+
+	//int a, b;
+	//cin >> a >> b;
+	//cout << a + b << endl << a - b << endl << a * b << endl << a / b << endl << a % b;
+
+	//int a, b, c;
+	//cin >>a >> b >> c;
+	//cout << (a + b) % c << endl <<
+	//		((a % c) + (b % c)) % c << endl <<
+	//		(a * b) % c << endl <<
+	//		((a % c) * (b % c)) % c;
+
+	//int a, b1,b2;
+	//cin >> a;
+	//cin >> b1;
+	//b2 = b1;
+	//int c[3] = {};
+	//for (int i = 2; i >= 0; i--) {
+	//	c[i] = int(b1) / (pow(10, i));
+	//	b1 = b1 - c[i] * pow(10, i);
+	//}
+	//for (int i = 0; i < 3; i++) {
+	//	cout << a * c[i]<<endl;
+	//}
+	//cout << a * b2;
+	
+	//return 0;
+}
