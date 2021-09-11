c6de8cf31660763d75d08890f6777acc38e69487
mad-kong <kcy0076@gmail.com>
2021-09-11 21:41:36 +0900

    baekjoon_step2

---
+++ baekjoon_step2/baekjoon_step2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	
+	//int a, b; cin >> a >> b;
+	//if (a > b) { cout << ">"; } else if (a == b) { cout << "=="; } else { cout << "<"; }
+	
+	//int a; cin >> a;
+	//if (a >= 90) { cout << "A"; } else if (a >= 80) { cout << "B"; } else if (a >= 70) { cout << "C"; } 
+	//else if (a >= 60) { cout << "D"; } else { cout << "F"; }
+	
+	//int a; cin >> a;
+	//if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) { cout << 1; } else { cout << 0; }
+	
+	//int x, y; cin >> x; cin >> y;
+	//if (x > 0) {if (y > 0) { cout << 1; } else { cout << 4; }}
+	//else{ if (y > 0) { cout << 2; } else { cout << 3; } }
+
+	//int h, m; cin >> h >> m;
+	//if (h == 0) {
+	//	if (m >= 45) { cout << h << " " << m - 45; } else { cout << 23 << " " << 60 - (45 - m); }
+	//}
+	//else {
+	//	if (m >= 45) { cout << h << " " << m - 45; } else { cout << h - 1 << " " << 60 - (45 - m); }
+	//}
+	
+	//return 0;
+}
