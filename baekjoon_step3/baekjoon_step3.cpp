a6250b9a379d31850ade84a55a21b9c1069fa11b
mad-kong <kcy0076@gmail.com>
2021-09-16 15:39:03 +0900

    baeckjoon_step3,4

---
+++ baekjoon_step3/baekjoon_step3.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	//int n; cin >> n;
+	//for (int i = 1; i < 10; i++) { cout << n << " * " << i << " = " << n * i << endl; }
+
+	//int testcase; cin >> testcase;
+	//for (int t = 0; t < testcase; t++) {int a, b; cin >> a >> b; cout << a + b << endl;}
+
+	//int n, sum = 0; cin >> n;
+	//for (int i = 1; i <= n; i++) { sum += i; } cout << sum;
+
+	//cin.tie(NULL);
+	//ios_base :: sync_with_stdio(false);	
+	//int testcase,a,b; cin >> testcase;
+	//for (int t = 0; t < testcase; t++) { cin >> a >> b; cout << a + b << "\n";}
+
+	//cin.tie(NULL);
+	//ios_base :: sync_with_stdio(false);	
+	//int n; cin >> n; 
+	//for (int i = 1; i <= n; i++) { cout << i << "\n"; }
+
+	//cin.tie(NULL);
+	//ios_base :: sync_with_stdio(false);	
+	//int n; cin >> n; 
+	//for (int i = n; i >= 1; i--) { cout << i << "\n"; }
+
+	//cin.tie(NULL);
+	//ios_base :: sync_with_stdio(false);
+	//int testcase, a, b; cin >> testcase;
+	//for (int t = 0; t < testcase; t++) { cin >> a >> b; cout << "Case #"<<t+1<<": "<< a + b << "\n";}
+
+	//cin.tie(NULL);
+	//ios_base::sync_with_stdio(false);
+	//int testcase, a, b; cin >> testcase;
+	//for (int t = 0; t < testcase; t++) {
+	//	cin >> a >> b; cout << "Case #" << t + 1 << ": "
+	//		<< a << " + " << b << " = " << a + b << "\n";
+	//}
+
+	//int n; cin >> n;
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = 0; j <= i; j++) { cout << "*"; }
+	//	cout << endl;
+	//}
+
+	//int n; cin >> n;
+	//for (int i = 0; i < n; i++) {
+	//	for (int j = 1; j < n - i; j++) { cout << " "; }
+	//	for (int j = 0; j <= i; j++) { cout << "*"; }
+	//	cout << endl;
+	//}
+
+	//int N; int X; int arr[10000];
+
+	//cin >> N >> X;
+	//for (int i = 0; i < N; i++) { cin >> arr[i]; }
+
+	//for (int j = 0; j < N; j++) {
+	//	if (arr[j] < X) { cout << arr[j]<< ' '; }
+	//	else
+	//		continue;
+	//}
+
+
+
+	return 0;
+}
---
+++ baekjoon_step4/baekjoon_step4.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//while (true){
+	//	int a, b; cin >> a >> b;
+	//	if (a == 0 && b == 0) { break; }
+	//	cout << a + b << endl;;
+	//}
+
+	//int a, b, count=0;
+	//while (true) {
+	//	if (cin >> a >> b && (a > 0 && b < 10)) { cout << a + b << endl; }
+	//	else { break; }	}
+
+	//int num, numcpy; cin >> num; numcpy = num;
+	//int count = 0, a, b;
+	//while (true) {
+	//	int temp = num;
+	//	if (num >= 10) {
+	//		a = temp / 10;
+	//		temp -= a * 10;
+	//		b = temp;
+	//		num = b * 10 + (a + b)%10;
+	//	}
+	//	else {
+	//		a = 0;
+	//		b = temp;
+	//		num = b * 10 + (a + b)%10;
+	//	}
+	//	count++;
+	//	if (num == numcpy) {
+	//		break;
+	//	}
+	//}
+	//cout << count;
+}
